30f360a14a0b9f0fae4dcd13b6fed89b5278cdd0
Fwarszawiak <fwarszawiakgostanian@udesa.edu.ar>
2025-11-18 13:55:48 -0300

    corregi errores html register(register y logins js casi terminados)

--- js/register.js
+++ js/register.js
@@ -1,19 +1,22 @@
-let formulario = document.querySelector("form");
-let campoEmail = document.querySelector("#correo");
-let campoPass = document.querySelector("#pass");
-let campoPassRepeat = document.querySelector("#passw");
+window.addEventListener("load", function () {
+    let formulario = document.querySelector(".caja-register");
+    let campoEmail = document.querySelector("#correo");
+    let campoPass = document.querySelector("#pass");
+    let campoPassRepeat = document.querySelector("#passw");
 
-formulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (campoEmail.value == "") {
-    alert("El campo email es obligatorio");
-  } else if (campoPass.value == "") {
-    alert("El campo contraseña es obligatorio");
-  } else if (campoPass.value.length < 6) {
-    alert("La contraseña debe tener al menos 6 caracteres");
-} else if (campoPass.value != campoPassRepeat.value) {
-    alert("Las contraseñas no coinciden");
-  } else {
-  this.submit();
-  }
+    formulario.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+    if (campoEmail.value == "") {
+        alert("El campo email es obligatorio");
+    } else if (campoPass.value == "") {
+        alert("El campo contraseña es obligatorio");
+    } else if (campoPass.value.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres");
+    } else if (campoPass.value != campoPassRepeat.value) {
+        alert("Las contraseñas no coinciden");
+    } else {
+        this.submit(); 
+    }
+});
 });
