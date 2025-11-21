a111165ffa12a74c51c7da8cdee68f249ccba5ee
bzapata-ops <bzapata@udesa.edu.ar>
2025-11-21 11:26:11 -0300

    search result termiando

--- js/search-results.js
+++ js/search-results.js
@@ -15,24 +15,19 @@ let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 let buscador = queryStringObj.get('buscador');
 
-formbuscador.addEventListener('submit', function(event)
-{
+formbuscador.addEventListener('submit', function(event) {
     event.preventDefault();
-    if (inputBuscar.value == ''){
-    alert("El campo de búsqueda no puede estar vacío");
-} else if (inputBuscar.value.length < 3) {
-    alert("El término buscado debe tener al menos 3 caracteres");
-
-}
-else {
-    this.submit();
-}
-
+    if (inputBuscar.value == '') {
+        alert("El campo de búsqueda no puede estar vacío");
+    } else if (inputBuscar.value.length < 3) {
+        alert("El término buscado debe tener al menos 3 caracteres");
+    } else {
+        this.submit();
+    }
 });
 
 
-
-let URLcategorias = 'https://dummyjson.com/products/categories'
+let URLcategorias = 'https://dummyjson.com/products/categories';
 let categorias = document.querySelector('.categorias');
 
 fetch(URLcategorias)
@@ -44,13 +39,51 @@ fetch(URLcategorias)
         for (let i = 0; i < data.length; i++) {
             let categoria = data[i];
             categorias.innerHTML += `
-            <li><a href="./category.html?categoria=${categoria.url}">${categoria.name}</a></li>
+                <li><a href="./category.html?categoria=${categoria}">${categoria}</a></li>
             `;
-            console.log(categoria.url);
         }
-        
-            
     })
     .catch(function(error) {
-        console.log("Error fetching products: " + error);
+        console.log("Error fetching categories: " + error);
     });
+
+
+let itemsContainer = document.querySelector('.items');
+let mensajeSinResultado = document.querySelector('.sin-resultado');
+
+itemsContainer.innerHTML = "";
+mensajeSinResultado.style.display = "none";
+
+if (buscador !== null && buscador !== "") {
+
+    fetch(`https://dummyjson.com/products/search?q=${buscador}`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+
+            console.log(data);
+
+    if (data.products.length == 0) {
+      mensajeSinResultado.style.display = "block";
+    }
+
+     for (let i = 0; i < data.products.length; i++) {
+
+                let producto = data.products[i];
+
+                itemsContainer.innerHTML += `
+                    <article class="item-card">
+                        <img class="item-image" src="${producto.thumbnail}" alt="">
+                        <h2 class="item-title">${producto.title}</h2>
+                        <p class="item-description">${producto.description}</p>
+                        <p class="item-price">$${producto.price} USD</p>
+                        <a class="verDetalle" href="./product.html?id=${producto.id}">ver detalle</a>
+                    </article>
+                `;
+            }
+        })
+        .catch(function(error) {
+            console.log("Error fetching search results: " + error);
+        });
+}
